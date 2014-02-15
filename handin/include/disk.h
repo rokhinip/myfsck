bf00d9897af34fc80a90f507e02a1c98bb5be191
Yifan Gu <guyifan1121@gmail.com>
2014-02-15 02:14:39 -0500

    finish part II

--- handin/include/disk.h
+++ handin/include/disk.h
@@ -3,12 +3,21 @@
 
 #include "ext2_fs.h"
 
+typedef struct block_slice_s {
+        int *array;
+
+        // length and capcity
+        int len;
+        int cap;
+}block_slice_t;
+
 // struct for one block group
 typedef struct group_s {
         int id;
         struct ext2_group_desc *desc;
         char *block_bitmap;
         char *inode_bitmap;
+        int entry_count;
         struct ext2_inode **inode_table;
 }group_t;
 
@@ -38,5 +47,23 @@ int open_disk(char *path, disk_t *disk);
 
 // print partition info
 void print_partitions(disk_t *disk, int partition_number);
+void print_all_groups_desc(disk_t *disk);
+void verify_all_blocks_allocated(disk_t *disk);
+void verify_all_inodes_allocated(disk_t *disk);
+void print_dir_info(struct ext2_dir_entry_2 *dir);
+void print_ls(partition_t *pt, char *path);
+void verify_file_block_allocated(partition_t *pt, int inode);
+void print_inode(partition_t *pt, int inode);
+
+// get attributes
+int is_dir(partition_t *pt, int inode_id);
+int get_dir(partition_t *pt, int inode_id, struct ext2_dir_entry_2 *dir);
+int next_dir(struct ext2_dir_entry_2 *dir, struct ext2_dir_entry_2 *next);
+int block_allocated(partition_t *pt, int block_number);
+int inode_allocated(partition_t *pt, int inode_number);
+int get_symbolic_path(partition_t *pt, int inode_id, char *path);
+
+// others
+int search_file(partition_t *pt, char *path, struct ext2_dir_entry_2 *ret);
 
 #endif
--- handin/src/disk.c
+++ handin/src/disk.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <errno.h>
 #include <error.h>
 #include <fcntl.h>
@@ -98,7 +99,7 @@ static inline int get_inodes_per_group(partition_t *pt)
         return pt->super_block->s_inodes_per_group;
 }
 
-static char * read_block(partition_t *pt, int block_index)
+static char * read_block(partition_t *pt, int block_index, int count)
 {
         int block_size = get_block_size(pt);
         if (block_size < 0) {
@@ -113,12 +114,12 @@ static char * read_block(partition_t *pt, int block_index)
 
         int sectors_per_block = block_size / sector_size_bytes;
 
-        char *buf = (char *)malloc(block_size);
+        char *buf = (char *)malloc(block_size*count);
         if (!buf) {
                 error_at_line(-1, errno, __FILE__, __LINE__, NULL);
         }
 
-        read_sectors(sector_offset, sectors_per_block, buf);
+        read_sectors(sector_offset, sectors_per_block*count, buf);
 
         return buf;
 }
@@ -199,6 +200,164 @@ int inode_allocated(partition_t *pt, int inode_number)
         return allocated(pt->groups[group_number]->inode_bitmap, inode_offset_in_group);
 }
 
+struct ext2_inode * get_inode_entry(partition_t *pt, int inode_id)
+{
+        int inodes_per_group = get_inodes_per_group(pt);
+
+        // inode start from 1
+        int group_number = (inode_id - 1) / inodes_per_group;
+        int inode_offset_in_group = (inode_id - 1) % inodes_per_group;
+
+        return pt->groups[group_number]->inode_table[inode_offset_in_group];
+}
+
+int is_dir(partition_t *pt, int inode_id)
+{
+        struct ext2_inode *inode = get_inode_entry(pt, inode_id);
+        return (inode->i_mode & EXT2_S_IFDIR) != 0;
+}
+
+int is_symbol(partition_t *pt, int inode_id)
+{
+        struct ext2_inode *inode = get_inode_entry(pt, inode_id);
+        return (inode->i_mode & EXT2_S_IFLNK) != 0;
+}
+
+int get_dir(partition_t *pt, int inode_id, struct ext2_dir_entry_2 *dir)
+{
+        struct ext2_inode *inode = get_inode_entry(pt, inode_id);
+
+        char *block = read_block(pt, inode->i_block[0], 1);
+        memcpy(dir, block, sizeof(*dir));
+        free(block);
+
+        return 0;
+}
+
+int append(block_slice_t *slice, int item)
+{
+        if (slice->len >= slice->cap) {
+                fprintf(stderr, "append failed: exceed capcity\n");
+                return -1;
+        }
+        slice->array[slice->len] = item;
+        slice->len++;
+
+        return slice->len;
+}
+
+int get_indirect_block(block_slice_t *slice, partition_t *pt, int block_id)
+{
+        int entries_per_block = get_block_size(pt) / 4; // 32-bit int
+        int *block = (int *)read_block(pt, block_id, 1);
+        int i;
+
+        for (i = 0; i < entries_per_block; i++) {
+                if (block[i] == 0) {
+                        return 0;
+                }
+                append(slice, block[i]);
+        }
+
+        free(block);
+        return i;
+}
+
+int get_double_indirect_block(block_slice_t *slice, partition_t *pt, int block_id)
+{
+        int entries_per_block = get_block_size(pt) / 4; // 32-bit int
+        int *indirect_block = (int *)read_block(pt, block_id, 1);
+        int i;
+
+        for (i = 0; i < entries_per_block; i++) {
+                if (indirect_block[i] == 0) {
+                        return 0;
+                }
+                int ret = get_indirect_block(slice, pt, indirect_block[i]);
+                if (ret == 0) {
+                        return 0;
+                }
+        }
+
+        free(indirect_block);
+        return i;
+}
+
+int get_triple_indirect_block(block_slice_t *slice, partition_t *pt, int block_id)
+{
+        int entries_per_block = get_block_size(pt) / 4; // 32-bit int
+        int *double_indirect_block = (int *)read_block(pt, block_id, 1);
+        int i;
+
+        for (i = 0; i < entries_per_block; i++) {
+                if (double_indirect_block[i] == 0) {
+                        return 0;
+                }
+                int ret = get_double_indirect_block(slice, pt, double_indirect_block[i]);
+                if (ret == 0) {
+                        return 0;
+                }
+        }
+
+        free(double_indirect_block);
+        return i;
+}
+
+block_slice_t * get_block_slice(partition_t *pt, int inode_id)
+{
+        struct ext2_inode *inode = get_inode_entry(pt, inode_id);
+        int cap = inode->i_blocks / (2 << pt->super_block->s_log_block_size);
+
+        block_slice_t *slice;
+        NEW_INSTANCE(slice, block_slice_t);
+
+        slice->cap = cap;
+        slice->len = 0;
+        slice->array = malloc(sizeof(int) * slice->cap);
+        if (!slice->array) {
+                error_at_line(-1, errno, __FILE__, __LINE__, NULL);
+        }
+
+        for (int i = 0; i < 12; i++) {
+                if (inode->i_block[i] == 0) {
+                        return slice;
+                }
+                append(slice, inode->i_block[i]);
+        }
+
+        if (get_indirect_block(slice, pt, inode->i_block[12]) == 0) {
+                return slice;
+        }
+        if (get_double_indirect_block(slice, pt, inode->i_block[13]) == 0) {
+                return slice;
+        }
+        get_triple_indirect_block(slice, pt, inode->i_block[14]);
+
+        return slice;
+}
+
+static int load_inode_table(partition_t *pt, int group_id)
+{
+        group_t * g = pt->groups[group_id];
+
+        g->entry_count = get_inodes_per_group(pt) - get_free_inodes_count(g);
+        g->inode_table = malloc(sizeof(struct ext2_inode *) * g->entry_count);
+        if (!g->inode_table) {
+                error_at_line(-1, errno, __FILE__, __LINE__, NULL);
+        }
+
+        int block_count = (g->entry_count * sizeof(struct ext2_inode) + (get_block_size(pt) - 1)) /  get_block_size(pt);
+        char *table = read_block(pt, get_inode_table_bid(g), block_count);
+
+        for (int i = 0; i < g->entry_count; i++) {
+                NEW_INSTANCE(g->inode_table[i], struct ext2_inode);
+                memcpy(g->inode_table[i], table+i*sizeof(struct ext2_inode), sizeof(struct ext2_inode));
+        }
+        free(table);
+
+        return 0;
+}
+
 static int load_groups(partition_t *pt)
 {
         char buf[sector_size_bytes];
@@ -216,7 +375,7 @@ static int load_groups(partition_t *pt)
                 error_at_line(-1, errno, __FILE__, __LINE__, NULL);
         }
 
-        char *group_desc_table = read_block(pt, group_desc_block_offset); // read group descriptor table
+        char *group_desc_table = read_block(pt, group_desc_block_offset, 1); // read group descriptor table
 
         // load group descriptor and data for each group
         for (int i = 0; i < pt->group_count; i++) {
@@ -230,11 +389,11 @@ static int load_groups(partition_t *pt)
                 pt->groups[i]->id = i;
 
                 // get bitmaps
-                pt->groups[i]->block_bitmap = read_block(pt, get_block_bitmap_bid(pt->groups[i]));
-                pt->groups[i]->inode_bitmap = read_block(pt, get_inode_bitmap_bid(pt->groups[i]));
+                pt->groups[i]->block_bitmap = read_block(pt, get_block_bitmap_bid(pt->groups[i]), 1);
+                pt->groups[i]->inode_bitmap = read_block(pt, get_inode_bitmap_bid(pt->groups[i]), 1);
 
                 // get inode table
-                pt->groups[i]->inode_table = (struct ext2_inode **)read_block(pt, get_inode_table_bid(pt->groups[i]));
+                load_inode_table(pt, i);
         }
         free(group_desc_table);
 
@@ -257,11 +416,6 @@ int open_disk(char *path, disk_t *disk)
                         load_groups(disk->partitions[i]);
                 }
         }
-
-        //print_all_groups_desc(disk);
-        //printf("\n");
-        //verify_all_blocks_allocated(disk);
-        verify_all_inodes_allocated(disk);
         return 0;
 }
 
@@ -365,4 +519,196 @@ void print_partitions(disk_t *disk, int partition_number)
         partition_t *p = disk->partitions[partition_number-1];
         struct partition *pinfo = p->partition_info;
         printf("0x%02x %d %d\n", pinfo->sys_ind, pinfo->start_sect + p->base_sector, pinfo->nr_sects);
+}
+
+void print_dir_info(struct ext2_dir_entry_2 *dir)
+{
+        char name[EXT2_NAME_LEN];
+        printf("====== dir info ======\n");
+        printf("inode: %d\n", dir->inode);
+        printf("rec_len : %d\n", dir->rec_len);
+        printf("name_len: %d\n", dir->name_len);
+        printf("file_type: 0x%X\n", dir->file_type);
+        strncpy(name, dir->name, dir->name_len);
+        name[dir->name_len] = 0;
+        printf("name: %s\n", name);
+}
+
+void print_inode(partition_t *pt, int inode_id)
+{
+        struct ext2_inode *inode = get_inode_entry(pt, inode_id);
+        printf("====== inode info ======\n");
+        printf("inode id %d\n", inode_id);
+        printf("mode: 0x%X\n", inode->i_mode);
+        printf("block count: %d\n", inode->i_blocks);
+}
+
+int get_symbolic_path(partition_t *pt, int inode_id, char *path)
+{
+        if (!is_symbol(pt, inode_id)) {
+                fprintf(stderr, "not a symbolic link\n");
+                return -1;
+        }
+
+        struct ext2_inode *inode = get_inode_entry(pt, inode_id);
+        if (inode->i_blocks == 0) {
+                memcpy(path, inode->i_block, sizeof(inode->i_block));
+                return 0;
+        }
+        
+        printf("too long symbol, not going to handle this moment, i_block: %d\n", inode->i_blocks);
+        
+        return -1;
+}
+
+void print_slice(block_slice_t *slice)
+{
+        printf("cap: %d\n", slice->cap);
+        printf("len: %d\n", slice->len);
+        printf("blocks:\n");
+        for (int i = 0; i < slice->len; i++) {
+                printf("%d ", slice->array[i]);
+        }
+        printf("\n");
+}
+
+void list_dir_in_block(partition_t *pt, int block_id)
+{
+        int block_size = get_block_size(pt);
+        char *block = read_block(pt, block_id, 1);
+        int offset = 0;
+        struct ext2_dir_entry_2 dir;
+
+        for (;;) {
+                if (offset >= block_size) {
+                        break;
+                }
+
+                memcpy(&dir, block+offset, sizeof(dir));
+                if (dir.rec_len == 0) {
+                        break;
+                }
+                print_dir_info(&dir);
+
+                offset += dir.rec_len;
+        }
+        free(block);
+}
+
+void print_ls(partition_t *pt, char *path)
+{
+        printf("ls %s\n", path);
+        struct ext2_dir_entry_2 dir;
+        
+        int inode = search_file(pt, path, &dir);
+        if (inode == 0) {
+                return;
+        }
+        //struct ext2_inode *inode = get_inode_entry(pt, dir->inode);
+        block_slice_t *slice = get_block_slice(pt, dir.inode);
+        print_slice(slice);
+
+        for (int i = 0; i < slice->len; i++) {
+                list_dir_in_block(pt, slice->array[i]);
+        }
+        free(slice);
+}
+
+int find_child_in_block(partition_t *pt, int block_id, char *childname, struct ext2_dir_entry_2 *ret)
+{
+        int block_size = get_block_size(pt);
+        char *block = read_block(pt, block_id, 1);
+        int offset = 0;
+        struct ext2_dir_entry_2 dir;
+
+        int child_inode = 0;
+        for (;;) {
+                if (offset >= block_size) {
+                        break;
+                }
+
+                memcpy(&dir, block+offset, sizeof(dir));
+                if (dir.rec_len == 0) {
+                        break;
+                }
+                if (dir.name_len == strlen(childname) && strncmp(childname, dir.name, dir.name_len) == 0) {
+                        child_inode = dir.inode;
+                        memcpy(ret, &dir, sizeof(dir));
+                        break;
+                }
+                offset += dir.rec_len;
+        }
+        free(block);
+
+        return child_inode;
+}
+
+int find_child(partition_t *pt, struct ext2_dir_entry_2 *parent, char *childname, struct ext2_dir_entry_2 *ret)
+{
+        block_slice_t *slice = get_block_slice(pt, parent->inode);
+
+        int child_inode = 0;
+        for (int i = 0; i < slice->len; i++) {
+                print_slice(slice);
+                child_inode = find_child_in_block(pt, slice->array[i], childname, ret);
+                if (child_inode != 0) {
+                        break;
+                }
+        }
+        free(slice);
+        return child_inode;
+}
+
+int search_file(partition_t *pt, char *path, struct ext2_dir_entry_2 *ret)
+{
+        char *saveptr;
+        struct ext2_dir_entry_2 dir;
+
+        if (path[0] != '/') {
+                printf("need absolute path\n");
+                return -1;
+        }
+
+        // copy the dir name to avoid segfault caused by strtok_r
+        char *str = malloc(strlen(path) + 1);
+        if (!str) {
+                error_at_line(-1, errno, __FILE__, __LINE__, NULL);
+        }
+        strcpy(str, path);
+
+        get_dir(pt, 2, &dir); // get root
+        int ret_inode = 0;
+        for (;;) {
+                char *token = strtok_r(str, "/", &saveptr);
+                str = NULL;
+                if (!token) {
+                        break;
+                }
+
+                ret_inode = find_child(pt, &dir, token, ret);
+                if (ret_inode == 0) {
+                        printf("not found\n");
+                        break;
+                }
+
+                print_inode(pt, ret_inode);
+                if (is_dir(pt, ret_inode)) {
+                        get_dir(pt, ret_inode, &dir);
+                }
+        }
+
+        free(str);
+
+        return ret_inode;
+}
+
+void verify_file_block_allocated(partition_t *pt, int inode)
+{
+        block_slice_t *slice = get_block_slice(pt, inode);
+        for (int i = 0; i < slice->len; i++) {
+                if (!block_allocated(pt, slice->array[i])) {
+                        printf("error data block[%d] not allocated in block_bitmap\n", slice->array[i]);
+                }
+        }
+        printf("ok! all data blocks allocated\n");
 }
